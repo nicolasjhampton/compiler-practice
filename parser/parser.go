f706f585ba91fc27aa4b3618c4a45f06830ad0a6
Nicolas Hampton <nicolasjhampton@gmail.com>
2018-04-16 22:55:12 -0700

    Unified arg parser

--- parser/parser.go
+++ parser/parser.go
@@ -17,14 +17,46 @@ func check(e error) {
 }
 
 type DefNode struct {
+	Name Node
+	ArgNames []Node
+	Body Node
+}
+
+func (d DefNode) isNode() int {
+	return 1
+}
+
+type CallNode struct {
+	Name Node
+	ArgExprs []Node
+}
+
+func (c CallNode) isNode() int {
+	return 1
+}
+
+type IdentifierNode struct {
 	Name string
-	ArgNames []string
-	Body IntegerNode
+	Ref Node
+}
+
+func (i IdentifierNode) isNode() int {
+	return 1
 }
 
 type IntegerNode struct {
 	Value int
 }
+
+func (i IntegerNode) isNode() int {
+	return 1
+}
+
+type Node interface {
+	isNode() int
+}
+
+type nodeFunc func(*Parser) Node
 
 func (p *Parser) Parse() *DefNode {
 	return p.ParseDef()
@@ -32,33 +64,41 @@ func (p *Parser) Parse() *DefNode {
 
 func (p *Parser) ParseDef() *DefNode {
 	_ = p.Consume("def")
-	identity := p.Consume("identifier")
-	args := p.ParseArgNames()
-	body := p.ParseExpr()
+	identity := ParseIdentifier(p)
+	fmt.Println(p.Tokens)
+	args := p.ParseArgs(ParseIdentifier) //p.ParseArgNames()
+	body := ParseExpr(p)
 	_ = p.Consume("end")
-	dnode := DefNode{ Name: identity.Value, ArgNames: args, Body: *body}
+	dnode := DefNode{ Name: identity, ArgNames: args, Body: body }
 	return &dnode
 }
 
-func (p *Parser) ParseExpr() *IntegerNode {
-	return p.ParseInteger()
+func ParseExpr(p *Parser) Node {
+	if p.Peek("integer", 0) {
+		return ParseInteger(p)
+	} else if p.Peek("identifier", 0) && p.Peek("oparen", 1) {
+		fmt.Println(p.Tokens)
+		return ParseCall(p)
+	} else if p.Peek("identifier", 0) {
+		return ParseIdentifier(p);
+	}
+	panic("No known token found in function body.")
 }
 
-func (p *Parser) ParseInteger() *IntegerNode {
-	intr := p.Consume("integer")
-	num, err := strconv.Atoi(intr.Value)
-	check(err)
-	iNode := IntegerNode{ Value: num }
-	return &iNode
+func ParseCall(p *Parser) CallNode {
+	identity := ParseIdentifier(p)
+	argExprs := p.ParseArgs(ParseExpr) //p.ParseArgExprs();
+	cNode := CallNode{ Name: identity, ArgExprs: argExprs }
+	return cNode
 }
 
-func (p *Parser) ParseArgNames() []string {
-	args := []string{}
+func (p *Parser) ParseArgs(fn nodeFunc) []Node {
+	args := []Node{}
+	fmt.Println(p.Tokens)
 	_ = p.Consume("oparen")
-	// Arguments here
-	for ;p.Peek("identifier"); {
-		args = append(args, p.Consume("identifier").Value)
-		if p.Peek("comma") {
+	for ;p.Peek("identifier", 0); {
+		args = append(args, fn(p))
+		if p.Peek("comma", 0) {
 			_ = p.Consume("comma")
 		}
 	}
@@ -66,8 +106,48 @@ func (p *Parser) ParseArgNames() []string {
 	return args
 }
 
-func (p *Parser) Peek(typeExpected string) bool {
-	return p.Tokens[0].Type == typeExpected
+// func (p *Parser) ParseArgExprs(fn nodeFunc) []Node {
+// 	args := []Node{}
+// 	_ = p.Consume("oparen")
+// 	for ;p.Peek("identifier", 0); {
+// 		args = append(args, p.ParseExpr())
+// 		if p.Peek("comma", 0) {
+// 			_ = p.Consume("comma")
+// 		}
+// 	}
+// 	_ = p.Consume("cparen")
+// 	return args
+// }
+
+// func (p *Parser) ParseArgNames() []IdentifierNode {
+// 	args := []IdentifierNode{}
+// 	_ = p.Consume("oparen")
+// 	for ;p.Peek("identifier", 0); {
+// 		args = append(args, p.ParseIdentifier())
+// 		if p.Peek("comma", 0) {
+// 			_ = p.Consume("comma")
+// 		}
+// 	}
+// 	_ = p.Consume("cparen")
+// 	return args
+// }
+
+func ParseIdentifier(p *Parser) Node {
+	identity := p.Consume("identifier")
+	iNode := IdentifierNode{ Name: identity.Value }
+	return iNode
+}
+
+func ParseInteger(p *Parser) IntegerNode {
+	intr := p.Consume("integer")
+	num, err := strconv.Atoi(intr.Value)
+	check(err)
+	iNode := IntegerNode{ Value: num }
+	return iNode
+}
+
+func (p *Parser) Peek(typeExpected string, offset int) bool {
+	return p.Tokens[offset].Type == typeExpected
 }
 
 func (p *Parser) Consume(typeExpected string) *t.Token {
